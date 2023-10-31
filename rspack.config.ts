662232196ed5c7ec5b82483209eda835cc331eed
hardfist <yangjianzju@gmail.com>
2023-10-31 19:15:26 +0800

    chore: fix rspack.config.js (#2)
    * chore: fix rspack.config.js
    
    * chore: fix title

---
+++ rspack.config.ts
@@ -0,0 +1,80 @@
+import type { Configuration } from "@rspack/cli";
+import path from "node:path";
+
+export default {
+  entry: {
+    main: "./src/index.jsx",
+  },
+  experiments: {
+    rspackFuture: {
+      disableTransformByDefault:true
+    }
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    alias: {
+      react: "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+      "react/jsx-runtime": "preact/jsx-runtime",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        type: 'asset'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'builtin:swc-loader',
+        options: {
+          sourceMap: true,
+          jsc: {
+            parser: {
+              syntax: 'ecmascript',
+              jsx:true
+            },
+             transform: {
+                react: {
+                  runtime: 'automatic'
+                },
+              },
+          },
+        },
+        type: 'javascript/auto',
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'builtin:swc-loader',
+        options: {
+          sourceMap: true,
+          jsc: {
+            parser: {
+              syntax: 'typescript',
+              jsx:true
+            },
+             transform: {
+                react: {
+                  runtime: 'automatic'
+                },
+              },
+          },
+        },
+        type: 'javascript/auto',
+      }
+    ]
+  },
+  builtins: {
+    html: [
+      {
+        template: "./index.html",
+        scriptLoading: "module",
+      },
+    ],
+  },
+  externalsType: 'script'
+} satisfies Configuration;
