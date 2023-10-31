340c5c1868a33d5e5b14eae35780feb1289700b0
chenhaoli <chenhaoli@bytedance.com>
2023-10-31 16:04:53 +0800

    chore: tweaks

--- rspack.config.js
+++ rspack.config.js
@@ -8,20 +8,80 @@ module.exports = {
   entry: {
     main: "./src/index.jsx",
   },
+  //   experiments: {
+  //     rspackFuture: {
+  //       disableTransformByDefault: true,
+  //     },
+  //   },
   mode: isDev ? "development" : "production",
   builtins: {
+    // react: {
+    //   pragma: "h",
+    //   pragmaFrag: "Fragment",
+    // },
     html: [{ template: "./index.html" }],
   },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-    resolve: {
-      alias: {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat", // Must be below test-utils
-        "react/jsx-runtime": "preact/jsx-runtime",
-      },
-    },
+  //   resolve: {
+  //     alias: {
+  //       react: "preact/compat",
+  //       "react-dom/test-utils": "preact/test-utils",
+  //       "react-dom": "preact/compat", // Must be below test-utils
+  //       "react/jsx-runtime": "preact/jsx-runtime",
+  //     },
+  //   },
+  //   module: {
+  //     rules: [
+  //       {
+  //         test: /\.jsx$/,
+  //         use: {
+  //           loader: "builtin:swc-loader",
+  //           options: {
+  //             jsc: {
+  //               parser: {
+  //                 syntax: "ecmascript",
+  //                 jsx: true,
+  //               },
+  //               transform: {
+  //                 react: {
+  //                   development: isDev,
+  //                   refresh: false,
+  //                   // Preact support
+  //                   // @see https://preactjs.com/guide/v10/getting-started#aliasing-in-webpack
+  //                   pragma: "h",
+  //                   pragmaFrag: "Fragment",
+  //                 },
+  //               },
+  //             },
+  //           },
+  //         },
+  //       },
+  //     ],
+  //   },
+  //   module: {
+  //     rules: [
+  //       {
+  //         test: /\.svg$/i,
+  //         issuer: /\.[jt]sx?$/,
+  //         use: [
+  //           /**
+  //            * @see https://react-svgr.com/docs/webpack/#use-your-own-babel-configuration
+  //            */
+  //           {
+  //             loader: "babel-loader",
+  //             options: {
+  //               presets: ["preact", "env"],
+  //             },
+  //           },
+  //           {
+  //             loader: "@svgr/webpack",
+  //             options: { babel: false },
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //   },
 };
